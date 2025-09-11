77b6b97161c4d14f29e6fcca44b3252fd46e5d8e
J <jordymeijer2712@gmail.com>
2025-09-11 10:39:38 +0200

    index0.1

--- src/index.php
+++ src/index.php
@@ -1,31 +1,29 @@
 <?php
 
-?>
+session_start();
+#
+#
+#
+exit();
 
-^^^^^^^^^
-php hier boven
+?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Croissant!</title>
+    <title>SCHOOL SYSTEM</title>
     <link rel="icon" type="image/x-icon" href="./img/croissant.webp">
     <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-<div class="header-container">
-        <h1>Croissant!</h1>
-</div>
     <img src="./img/croissant.webp" alt="Croissant Image" width="200">
-
-    <div class="footer-container">
-        
-    </div>
-    
 </body>
 
-<div class="submit">hey</div>
+<div class="submit">This button is for testing!</div>
+
+<div class="footer-container">/\/\/\/\/\/\/\</div>
 </html>
