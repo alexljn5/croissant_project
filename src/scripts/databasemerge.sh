1967912abb235acc39c47bf129609723bb564813
alexljn5 <alexljn5@gmail.com>
2025-09-23 10:49:03 +0200

    dd

--- src/scripts/databasemerge.sh
+++ src/scripts/databasemerge.sh
@@ -4,7 +4,7 @@
 # It runs in the background so you can keep working! Make sure your container is up.
 
 # Check if the Docker container is running
-if ! sudo docker ps | grep -q croissant_project-db-1; then
+if ! docker ps | grep -q croissant_project-db-1; then
     echo "Oh no! The container 'croissant_project-db-1' isn't running. Start it first, okay?"
     exit 1
 fi
@@ -19,6 +19,6 @@ if [ ! -f "$SQL_FILE" ]; then
 fi
 
 # Run the import in the background
-sudo docker exec -i croissant_project-db-1 mysql -u root -ppassword croissantdb < "$SQL_FILE" &
+docker exec -i croissant_project-db-1 mysql -u root -ppassword croissantdb < "$SQL_FILE" &
 
 echo "Yay! The merge started in the background. Check on it with 'sudo docker logs croissant_project-db-1' if you want!"
